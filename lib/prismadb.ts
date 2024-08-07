7cb741f68bc094f18eca6c4e46efd0affdbdc719
Jayant <jayant.sawarkar@purplezone.in>
2024-08-07 10:21:59 +0530

    new

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,12 +1,6 @@
-import { PrismaClient } from "@prisma/client"
+// lib/prismadb.js
+import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
+const prisma = new PrismaClient();
 
-const prismadb = globalThis.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb
-
-export default prismadb;
-
-
+export default prisma;
